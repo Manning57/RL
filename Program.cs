870befd9e6fee3be6cbddb976b57ac2a23787518
Landon <Landon.a.manning@conocophillips.com>
2024-03-26 15:38:14 -0500

    adding sentence randomizer

--- Program.cs
+++ Program.cs
@@ -3,22 +3,15 @@ using System;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using RL;
 
-string[] st = {
-    "You fucking suck!",
-    "Eat my ass!",
-    "I'm Huge!",
-    "Go Beef Go!"
-};
+var s = new sentence();
 
 var proc = Process.GetProcessesByName("RocketLeague").FirstOrDefault();
 if (proc != null && proc.MainWindowHandle != IntPtr.Zero)
     SetForegroundWindow(proc.MainWindowHandle);
 
-var random = new Random();
-var r = random.Next(0, st.Length);
-
-SendKeys.SendWait("t" + st[r] + "{ENTER}");
+SendKeys.SendWait(s.formSentence());
 
 [DllImport("user32")]
 static extern bool SetForegroundWindow(IntPtr hwnd);
---
+++ sentence.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RL
+{
+    internal class sentence
+    {
+        Random random = new Random();
+
+        public string[] st = {
+            "You fucking suck!",
+            "Eat my ass!",
+            "I'm Huge!",
+            "Go Beef Go!"
+        };
+
+        public string[] nouns = {
+            "face",
+            "ass",
+            "cock",
+            "belly",
+            "toes"
+        };
+
+        public string[] animals = {
+            "rhino",
+            "dog",
+            "gorilla",
+            "whale",
+            "donkey"
+        };
+
+        public string[] adjectives = {
+            "unconscious",
+            "in disbelief",
+            "in shambles",
+            "in love",
+            "paralized"
+        };
+
+        public string[] verbs = {
+            "slap",
+            "fiddle",
+            "torment",
+            "attack",
+            "stomp"
+        };
+
+        public string[] adverbs = {
+            "gently",
+            "aggressively",
+            "viciously",
+            "completely",
+            "timidly"
+        };
+
+        public string[] aggressor = {
+            "I'm",
+            "Beef is"
+        };
+
+        public String randomlySelect(string[] stringArray)
+        {
+            var choice = random.Next(0, stringArray.Length);
+            return stringArray[choice];
+        }
+
+        public String formSentence()
+        {
+            String s = "t" + randomlySelect(aggressor) +
+                " gonna " + randomlySelect(adverbs) +
+                " " + randomlySelect(verbs) +
+                " your " + randomlySelect(nouns) +
+                " like a " + randomlySelect(animals) +
+                " till you're " + randomlySelect(adjectives) +
+                "!{ENTER}";
+
+            return s;
+        }
+
+    }
+}
